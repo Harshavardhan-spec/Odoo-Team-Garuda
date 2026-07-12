c67d2978018ba60b49f827cf9f8f146a541daf8e
praveenmididoddi7 <praveenmididoddi7@gmail.com>
2026-07-12 12:14:35 +0530

    created database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,12 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project Apps
+    'vehicles',
+    'drivers',
+    'trips',
+    'maintenance',
+    'fuel',
+    'expenses',
+    'dashboard',
+    'reports',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +86,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'garuda',
+        'USER': 'postgres',
+        'PASSWORD': 'praveen@8985',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from vehicles.models import Vehicle
+from drivers.models import Driver
 
-# Create your models here.
+
+class Trip(models.Model):
+
+    STATUS_CHOICES = [
+        ("DRAFT", "Draft"),
+        ("DISPATCHED", "Dispatched"),
+        ("COMPLETED", "Completed"),
+        ("CANCELLED", "Cancelled"),
+    ]
+
+    source = models.CharField(max_length=100)
+
+    destination = models.CharField(max_length=100)
+
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.PROTECT,
+        related_name="trips"
+    )
+
+    driver = models.ForeignKey(
+        Driver,
+        on_delete=models.PROTECT,
+        related_name="trips"
+    )
+
+    cargo_weight = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    planned_distance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    actual_distance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    fuel_consumed = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    revenue = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="DRAFT"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.source} → {self.destination}"
